99c220d99e32d19f4c096faf9e632cfd892ba390
Henrique Ritter <henrique-ritter@hotmail.com>
2021-01-19 15:34:34 -0300

    model orphanage created/first insert OK

--- backend/src/database/migrations/1611077822013-create_orphanages.ts
+++ backend/src/database/migrations/1611077822013-create_orphanages.ts
@@ -39,6 +39,10 @@ export class createOrphanages1611077822013 implements MigrationInterface {
             type: "text",
           },
           {
+            name: "opening_hours",
+            type: "varchar",
+          },
+          {
             name: "open_on_weekends",
             type: "boolean",
             default: false,
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,47 @@
 import express from "express";
 import "./database/connection";
 
+//para realizar o vinculo entre o model e a tabela no banco
+import { getRepository } from "typeorm";
+import Orphanage from "./models/Orphanage";
+
 const app = express();
 
 //faz com que o express reconheca JSON
 app.use(express.json());
 
-app.get("/users", (request, response) => {
+// ASYNC por causa do await do save do repositorio
+app.post("/orphanages", async (request, response) => {
+  //pega os dados do orfanato do request body
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  } = request.body;
+
+  //transforma o orphanagesRepository em uma instancia que contem
+  // todos os metodos para criar, deletar etc.. dentro da tabela/model Orphanage
+  const orphanagesRepository = getRepository(Orphanage);
+
+  //cria mas nao salva no banco
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  });
+
+  //FUNCAO ASYNC!!!
+  //salva(commita) o objeto repositorio criado na aplicacao no banco
+  await orphanagesRepository.save(orphanage);
+
   return response.json({ mess: "hello world" });
 });
 
